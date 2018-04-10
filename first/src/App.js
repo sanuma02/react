4905b1113f207342e5fd7dddbb92546ba20e45d3
Sanchez Umana <fanny.sanchez.umana@intel.com>
2018-04-09 20:51:58 -0600

    adding event

--- first/src/App.js
+++ first/src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name:'Rex',age:5},
+      {name:'Gigi',age:1}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Click happened')
+  }
+
   render() {
     return (
       <div className="App">
         <p className="App-intro">
           Just a nice p to start.
         </p>
-        <Person name="Rex" age="5"/>
-        <Person name="Gigi" age="1">Hobbies: eat everything</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: eat everything</Person>
       </div>
     );
   }
