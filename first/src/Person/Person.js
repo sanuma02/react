6012e9a846d692bd9d5cba4ddb8a4dfe7307170e
Fanny <fanny.sanchez.umana@intel.com>
2018-04-13 11:01:25 -0600

    readme

--- first/src/Person/Person.js
+++ first/src/Person/Person.js
@@ -5,6 +5,7 @@ const person = (props) => {
 		<div>
 			<p onClick={props.click} >Hey {props.name}, happy {props.age} birthday!!!!!</p>
 			<p>{props.children}</p>
+			<input type="text" onChange={}/>
 		</div>
 		
 	)
